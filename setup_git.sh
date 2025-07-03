95373560a213b822065eb6020936881e08a1847d
Petrov Evgeny <PetrovE2@ya.ru>
2025-07-03 07:48:58 +0300

    update script, add colors

--- setup_git.sh
+++ setup_git.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
+### ЦВЕТА ##
+ESC=$(printf '\033') RESET="${ESC}[0m" MAGENTA="${ESC}[35m" RED="${ESC}[31m" GREEN="${ESC}[32m"
+
+### Функции цветного вывода ##
+magentaprint() { printf "${MAGENTA}%s${RESET}\n" "$1"; }
+errorprint() { printf "${RED}%s${RESET}\n" "$1"; }
+greenprint() { printf "${GREEN}%s${RESET}\n" "$1"; }
+
+
 # Функция для установки Git на Ubuntu
 install_git_ubuntu() {
+    echo; magentaprint "Installing Git on Ubuntu..."
     sudo apt update
     sudo apt install -y git
 }
 
 # Функция для установки Git на AlmaLinux
 install_git_almalinux() {
+    echo; magentaprint "Installing Git on AlmaLinux..."
     sudo dnf update -y
     sudo dnf install -y git
 }
 
 # Функция для установки глобальных настроек Git
 configure_git() {
+    echo; magentaprint "Configuring Git with provided username and email..."
     local username="$1"
     local email="$2"
 
@@ -36,23 +48,28 @@ install_git() {
         # AlmaLinux
         install_git_almalinux
     else
-        printf "Unsupported Linux distribution.\n" >&2
+        errorprint "Unsupported Linux distribution. Only Ubuntu and AlmaLinux are supported."
         return 1
     fi
 }
 
 # Основная функция
 main() {
+    # Проверка наличия аргументов
     if [ $# -ne 2 ]; then
-        printf "Usage: $0 <git-username> <git-email>\n" >&2
+        echo; magentaprint "Usage: $0 <git-username> <git-email>"; echo
         return 1
     fi
 
     install_git
     configure_git "$1" "$2"
-    printf "Git installation and configuration complete.\n"
-    # Вывести заданные изменения в конфигурации
+    echo; greenprint "Git installation and configuration complete."
+    
+    echo; magentaprint "Current Git configuration:"
+    # Вывод текущих настроек Git
     git config --list
+    echo; magentaprint "Git version:"
+    # Вывод версии Git
     git --version
 }
 
